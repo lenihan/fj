156a4c50eed5968fc4cc32cf2bdf0eb6b7755b83
David Lenihan <davidlenihan@outlook.com>
2025-07-21 08:21:31 -0500

    Title works

--- src/squareGraphicsView.cpp
+++ src/squareGraphicsView.cpp
@@ -32,7 +32,6 @@ SquareGraphicsView::SquareGraphicsView(QGraphicsScene* scene, QWidget* parent)
 
 void SquareGraphicsView::resizeEvent(QResizeEvent* event)
 {
-#if 1
     QScreen *screen = QApplication::primaryScreen();
     Q_ASSERT(screen);
     const qreal dpiX = screen->physicalDotsPerInchX();
@@ -45,66 +44,28 @@ void SquareGraphicsView::resizeEvent(QResizeEvent* event)
     // TODO: Need to update items in scene for new scene settings
 
     // Get the view's size
-    int width = viewport()->width();
-    int height = viewport()->height();
-
-    // Use the smaller dimension to keep the scene square
-    int side = qMin(width, height);
-
-    // Set the scene's rectangle to be square and centered
-    qreal offsetX = (width - side) / 2.0;
-    qreal offsetY = (height - side) / 2.0;
-    m_scene->setSceneRect(offsetX, offsetY, side, side);
-#else
-    {
-        auto vw = viewport()->width();
-        auto vh = viewport()->height();
-        auto w = width();
-        auto h = height();
-        // int i;
-        // i++;
-    }
-    QScreen *screen = QApplication::primaryScreen();
-    Q_ASSERT(screen);
-    const qreal dpiX = screen->physicalDotsPerInchX();
-    const qreal dpiY = screen->physicalDotsPerInchY();
-    scale(dpiX/dpiX, dpiY/dpiX);
-
-    // Get the view's size
     const int width_px = viewport()->width();
     const int height_px = viewport()->height();
- 
-    // Convert size to inches
-    const qreal width_in = width_px / dpiX;
-    const qreal height_in = height_px / dpiY;
-    
+
     // Use the smaller dimension to keep the scene square
-    const qreal smallerSide_in = width_in <= height_in ? width_in : height_in;
-    
+    const int side_px = width_px <= height_px ? width_px : height_px;
+    const qreal dpi = width_px <= height_px ? dpiX : dpiY;
+
     // Set the scene's rectangle to be square and centered
-    qreal offsetX_in = (width_in - smallerSide_in) / 2.0;
-    qreal offsetY_in = (height_in - smallerSide_in) / 2.0;
-    
-    const int offsetX_px = offsetX_in * dpiX;
-    const int offsetY_px = offsetY_in * dpiY;
-    const int sceneWidth_px = smallerSide_in * dpiX;
-    const int sceneHeight_px = smallerSide_in * dpiY;
-    
-    m_scene->setSceneRect(offsetX_px, offsetY_px, sceneWidth_px, sceneHeight_px);
-    m_sceneBorder->setRect(m_scene->sceneRect());
+    const qreal offsetX_px = (width_px - side_px) / 2.0;
+    const qreal offsetY_px = (height_px - side_px) / 2.0;
+    m_scene->setSceneRect(offsetX_px, offsetY_px, side_px, side_px);
 
-    // Scale view based on dpi\
-
-    // scale(2.0, 1.0);
-    
     // Set title
-    QWidget* mainWindow = window();
-    Q_ASSERT(mainWindow);
-    const qreal desired_in = 8.0;
-    const int percent = smallerSide_in / desired_in * 100.0;
-    const QString title = QString("FJ - %1\"x%1\" %2%").arg(desired_in).arg(percent);
-    mainWindow->setWindowTitle(title);
-#endif
+    {
+        QWidget* mainWindow = window();
+        Q_ASSERT(mainWindow);
+        const qreal desired_in = 8.0;
+        const qreal side_in = side_px / dpi;
+        const int percent = side_in / desired_in * 100.0;
+        const QString title = QString("FJ - %1\"x%1\" %2%").arg(desired_in).arg(percent);
+        mainWindow->setWindowTitle(title);
+    }
     QGraphicsView::resizeEvent(event);
 }
 
