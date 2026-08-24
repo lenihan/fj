5330dd8c1871bb04f5b0cd48922505b4b73bd044
David Lenihan <davidlenihan@outlook.com>
2026-08-23 21:27:11 -0500

    Delete the old Qt app source files
    Completes the removal started in 25f8799 -- CMakeLists.txt stopped
    referencing these, but the files themselves hadn't actually been
    deleted yet.

--- src/contentItem.cpp
+++
@@ -1,9 +0,0 @@
-#include "contentItem.h"
-#include <QPen>
-
-ContentItem::ContentItem(CardNumber cardNumber, Year year, QGraphicsItem* parent)
-    : CardItem(cardNumber, year, parent)
-{
-    setupBackground();
-    setupLines();
-}
